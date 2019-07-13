a0cff554e852054b106de55067bc60ad6b363f45
Svet <stsupryk@gmail.com>
2019-07-13 10:10:14 +0300

    add scrolling function

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 (function () {
     // invocation
 
-    Tween();
+    // Tween();
     scrollMagic();
     ChoosePrice();
     sendPhone();
@@ -9,57 +9,219 @@
 
     /////Gsap
 
-    function Tween() {
-        let tl = new TimelineMax();
-        tl
-            .from('#left', 2, {
-                x: '-300%',
-                ease: Bounce.easeOut
-            }, delay = 1)
-            .from('#right', 2, {
-                x: '300%',
-                ease: Bounce.easeOut
-            }, "-=2")
-            .from('.wellcome__btn', .5, {
-                borderRadius: "30px"
-            })
-            .from('.logo__h1', 1, {
-                y: '-100',
-                opacity: 0
-            }, "-=0.5")
-            .staggerFrom(".nav__list", .5, {
-                y: '-50',
-                opacity: 0
-            }, 0.1, "-=0.25")
-    }
-
     function scrollMagic() {
-        // init controller
-        var controller = new ScrollMagic.Controller();
-
-        var tween = TweenMax.from('#about', 1, {
-            y: 500,
-            opacity: 0
+        console.log(com.greensock.plugins.ScrollToPlugin.version);
+        // Init controller
+        var controller = new ScrollMagic.Controller({
+            globalSceneOptions: {
+                duration: $('.screen').height(),
+                triggerHook: .025,
+                reverse: true
+            },
+            vertical: false
         });
-        var tween1 = TweenMax.from('#contacts', 1, {
-            y: 500,
-            opacity: 0
-        });
-        // create a scene
 
-        var scene = new ScrollMagic.Scene({
-                triggerElement: "#servises",
-                offset: 400
-            })
-            .setTween(tween)
-            .addTo(controller);
-        var scene2 = new ScrollMagic.Scene({
-                triggerElement: "#clients",
-                offset: 300
-            })
-            .setTween(tween1)
-            .addTo(controller);
+
+        var scenes = {
+
+            'scene2': {
+                'screen1': 'anchor1'
+            },
+            'scene3': {
+                'screen2': 'anchor2'
+            },
+            'scene4': {
+                'screen3': 'anchor3'
+            }
+        }
+
+        for (var key in scenes) {
+            if (!scenes.hasOwnProperty(key)) continue;
+            var obj = scenes[key];
+            for (var prop in obj) {
+                if (!obj.hasOwnProperty(prop)) continue;
+
+                new ScrollMagic.Scene({
+                        triggerElement: '#' + prop
+                    })
+                    .setClassToggle('#' + obj[prop], 'active')
+                    .addTo(controller);
+            }
+        }
+
+        controller.scrollTo(function (target) {
+
+            TweenMax.to(window, 0.5, {
+                scrollTo: {
+                    x: target,
+                    autoKill: true // Allow scroll position to change outside itself
+                },
+                ease: Cubic.easeInOut
+            });
+
+        });
+
+        var anchor_nav = document.querySelector('.anchor-nav');
+
+        anchor_nav.addEventListener('click', function (e) {
+            var target = e.target,
+                id = target.getAttribute('href');
+
+            if (id !== null && id.length > 0) {
+                e.preventDefault();
+                controller.scrollTo(id);
+
+                if (window.history && window.history.pushState) {
+                    history.pushState("", document.title, id);
+                }
+            }
+        });
+
+        var $navs = $('.anchor-nav a');
+        window.addEventListener("wheel", onWheel);
+
+        function onWheel(event) {
+
+            var normalized;
+            var delta = event.wheelDelta;
+            var scroll = (window.pageXOffset || document.scrollLeft) - (document.clientLeft || 0) || 0;
+
+            if (delta) {
+                normalized = (delta % 120) == 0 ? delta / 120 : delta / 12;
+            } else {
+                delta = event.deltaY || event.detail || 0;
+                normalized = -(delta % 3 ? delta * 10 : delta / 3);
+            }
+
+            var currentIndex = $navs.index($('.active'));
+            var newIndex;
+            newIndex = normalized > 0 ? currentIndex + 1 : currentIndex - 1;
+
+            if (newIndex >= 0 && newIndex < $navs.length) {
+                $navs.eq(newIndex)[0].click()
+            }
+        }
     }
+
+
+    // function Tween() {
+    //     let tl = new TimelineMax();
+    //     tl
+    //         .from('#left', 2, {
+    //             x: '-300%',
+
+    //         }, delay = 1)
+    //         .from('#right', 2, {
+    //             x: '300%',
+
+    //         }, "-=2")
+    //         .from('.wellcome__btn', .5, {
+    //             borderRadius: "30px"
+    //         })
+    //         .from('.logo__h1', 1, {
+    //             y: '-100',
+    //             opacity: 0
+    //         }, "-=0.5")
+    //         .staggerFrom(".nav__list", .5, {
+    //             y: '-50',
+    //             opacity: 0
+    //         }, 0.1, "-=0.25")
+    // }
+
+    // function scrollMagic() {
+    //     // init controller
+    //     let dur = document.querySelector(".screen").offsetWidth;
+    //     console.log(window.TweenMax);
+
+    //     var controller = new ScrollMagic.Controller({
+    //         globalSceneOptions: {
+    //             duration: dur,
+    //             triggerHook: .025,
+    //             reverse: true
+    //         },
+    //         vertical: false,
+    //     });;
+
+    //     var tween = TweenMax.fromTo('#screen1', 1, {
+    //         x: 0
+    //     }, {
+    //         x: -100
+    //     });
+    //     var tween1 = TweenMax.to('#screen2', 1, {
+    //         x: 0
+    //     });
+    //     var tween2 = TweenMax.fromTo('#screen2', 1, {
+    //         x: 0
+    //     }, {
+    //         x: -100
+    //     });
+    //     var tween3 = TweenMax.to('#screen3', 1, {
+    //         x: 0
+    //     });
+    //     // create a scene
+
+    //     var scene = new ScrollMagic.Scene({
+    //             triggerElement: '#screen2', // starting scene, when reaching this element
+    //             // offset: 400
+    //         })
+    //         .setTween(tween)
+    //         .setTween(tween1)
+    //         .addTo(controller);
+    //     var scene2 = new ScrollMagic.Scene({
+    //             triggerElement: '#screen3', // starting scene, when reaching this element
+    //             // offset: 300
+    //         })
+    //         .setTween(tween2)
+    //         .setTween(tween3)
+    //         .addTo(controller);
+    // }
+    // function Tween() {
+    //     let tl = new TimelineMax();
+    //     tl
+    //         .from('#left', 2, {
+    //             x: '-300%',
+    //             ease: Bounce.easeOut
+    //         }, delay = 1)
+    //         .from('#right', 2, {
+    //             x: '300%',
+    //             ease: Bounce.easeOut
+    //         }, "-=2")
+    //         .from('.wellcome__btn', .5, {
+    //             borderRadius: "30px"
+    //         })
+    //         .from('.logo__h1', 1, {
+    //             y: '-100',
+    //             opacity: 0
+    //         }, "-=0.5")
+    //         .staggerFrom(".nav__list", .5, {
+    //             y: '-50',
+    //             opacity: 0
+    //         }, 0.1, "-=0.25")
+    // }
+
+    //     var tween = TweenMax.from('#about', 1, {
+    //         y: 500,
+    //         opacity: 0
+    //     });
+    //     var tween1 = TweenMax.from('#contacts', 1, {
+    //         y: 500,
+    //         opacity: 0
+    //     });
+    //     // create a scene
+
+    //     var scene = new ScrollMagic.Scene({
+    //             triggerElement: "#servises",
+    //             offset: 400
+    //         })
+    //         .setTween(tween)
+    //         .addTo(controller);
+    //     var scene2 = new ScrollMagic.Scene({
+    //             triggerElement: "#clients",
+    //             offset: 300
+    //         })
+    //         .setTween(tween1)
+    //         .addTo(controller);
+    // }
 
 
     /////// choosing the price of site type
